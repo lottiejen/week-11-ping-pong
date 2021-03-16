339761aa2c3610ffde9dd3688b55820ccc32c71d
Lottie <lottiej17@gmail.com>
2021-03-16 10:43:00 +0000

    Increment working, shows player 1 +2 scores with click, reset buttton working. All working.

--- src/index.js
+++ src/index.js
@@ -3,59 +3,59 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-// initial state 
-const initial = {
+const initialState = {
   player1: 0,
   player2: 0,
 };
 
-// REDUCER 
-
-// the reducer gets given the current version of the state
-// and the action that called it, which has a "type" property
-  const reducer = (state, action) => {
-
-  // depending on what the action's type property is
-  // we can do different things
-  switch (action.type) {
-
-  // using object spread to create a new object
-  // with the same properties
-  case "INCREMENT": return { ...state, value: state.value + 1 };
-  default: return state;
+// 
+// const changeReducer = (currentState, action) => {
+//   return 
+// }
+const reducer = (state, action) => {
+  switch (action.type){
+    case "INCREMENTPLAYER1" : return { ...state, player1: state.player1 + 1 }; 
+    case "INCREMENTPLAYER2" : return { ...state, player2: state.player2 + 1 };
+    case "RESET" : return initialState ; //  added reset action, need 2 setState to initialState // had { initialState } before, didn't reset showed as NaN on browser. 
+    default: return state;
   }
 };
 
 // store 
-
-const store = createStore(reducer, initial);
-
-  // we update subscribe to call the ReactDOM.render
-// method whenever the state changes
-  const render = () => {
-    let state = store.getState();
-  // pass in state.value as a value prop
-    ReactDOM.render(
-      <App />,
-      document.getElementById("root")
-    );
-  };
-  store.subscribe(render); // render when state changes
-  render(); // render when page loads using initial state
-
-
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  reducer, 
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
+const render = () => {
+  let state = store.getState();
+ReactDOM.render(
+  <React.StrictMode>
+        {/* we update subscribe to call the ReactDOM.render
+        method whenever the state changes */}
+        <App 
+        player1 = { state.player1 }
+        handleIncrementPlayer1={ () => store.dispatch({ type: "INCREMENTPLAYER1" })} 
+        player2 = {state.player2} 
+        handleIncrementPlayer2={ () => store.dispatch({ type: "INCREMENTPLAYER2" })}
+        handleReset={ () => store.dispatch({ type: "RESET" })}
+        />
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
+};
+
+store.subscribe(render); /* render when state changes */
+
+
+render(); /*render when page loads using initial state */
+/* // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals */
 reportWebVitals();
+// dispatch, dispatches an action
+// createStore is the Redux part of it all
+//
