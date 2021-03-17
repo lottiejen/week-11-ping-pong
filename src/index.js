76d4e664c5e19c3e36b2bc8ed4d25dbf2718922c
Lottie <lottiej17@gmail.com>
2021-03-17 18:24:26 +0000

    redux serated to seprate files, render function remaining

--- src/index.js
+++ src/index.js
@@ -3,64 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-
-const initialState = {
-  player1: 0,
-  player2: 0,
-  serving: 1, 
-  winner: 0,  
-};
-
-const server = (state) => {
-  let total = state.player1 + state.player2
-  return {
-    ...state,
-    serving: Math.floor(total / 5) % 2 === 0 ? 1 : 2 // math.floor rounds number down  
-  }
-}
-
-const incrementPlayer1 = (state) => { // accepts state object
-  return {
-  ...state, // copying the state object, accessing property within 
-  player1: state.player1 + 1 // access the property (player1), state.player1 + 1 allows you to increment. 
-  }
-}
-
-const incrementPlayer2 = (state) => {
-  return {
-  ...state, 
-  player2: state.player2 + 1
-  }
-}
-
-const isThereAWinner = (state) => {
-  return state.player1 >= 21 || state.player2 >= 21 // isThereAWinner returns true or false 
-}
-
-const whoWins = (state)  => {
-  return {
-    ...state, 
-    winner: isThereAWinner(state) ? (state.player1 > state.player2 ? 1 : 2) : 0 // if a winner, compares the player scores in brackets // if false, will set winner property to 0. 
-  }
-}
+import store from './data/store'
 
 
-const reducer = (state, action) => {
-  switch (action.type){
-    case "INCREMENTPLAYER1" : return whoWins(server(incrementPlayer1(state)))
-    case "INCREMENTPLAYER2" : return whoWins(server(incrementPlayer2(state)))
-    case "RESET" : return initialState ; 
-    default: return state;
-  }
-};
-
-// store 
-const store = createStore(
-  reducer, 
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 const render = () => {
   let state = store.getState();
