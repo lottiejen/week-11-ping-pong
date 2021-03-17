267e697b4170d704522e4500e1bcc56dc7e845a6
Lottie <lottiej17@gmail.com>
2021-03-17 18:27:33 +0000

    Updated with props, added ScoreCard, Winner & Button

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Winner from './components/Winner';
+import ScoreCard from './components/ScoreCard';
 
 const App = ({player1, player2, handleIncrementPlayer1, handleIncrementPlayer2, handleReset, serving, winner}) => (
+    
   <>
       {/* header */}
       <header className="jumbotron mt-4 mb-0">
-          <h1>Ping Pong Party</h1>
+          <h1>Ping Pong Party 🏓</h1>
       </header>
+      
       {/* scores */}
       <div className="row mb-4">
-          <div className="col-md-6 mt-4">
-          <div className= {"card text-center " + (serving === 1 ? "bg-dark text-white" : "") }>
-                  <h5 className="card-header">Player 1</h5>
-                  <div className="card-body">
-                      <p className="card-text display-1">{ player1 }</p>
-                  </div>
-                  <div className="card-footer">
-                      <button className="form-control btn btn-success" 
-                              onClick={ handleIncrementPlayer1 }>+</button>
-                  </div>
-              </div>
-          </div>
-          <div className="col-md-6 mt-4">
-          <div className= {"card text-center " + (serving === 2 ? "bg-dark text-white" : "") }>
-                  <h5 className="card-header">Player 2</h5>
-                  <div className="card-body">
-                      <p className="card-text display-1">{ player2 }</p>
-                  </div>
-                  <div className="card-footer">
-                      <button className="form-control btn btn-success"
-                              onClick={ handleIncrementPlayer2 }>+</button>
-                  </div>
-              </div>
-          </div>
-      </div>
+      <ScoreCard 
+        serving={ serving === 1 }
+        handleIncrement={ handleIncrementPlayer1 }
+        score = { player1 }
+        playerTitle = "Player 1"
+        />
+        
+      <ScoreCard 
+        serving={ serving === 2 }
+        handleIncrement={ handleIncrementPlayer2 }
+        score = { player2 }
+        playerTitle = "Player 2"
+        />
+        </div>
+          
       { /* winner message */}
-      {winner !== 0 ? <h2 className="alert alert-success">Player { winner } Wins!</h2> : null }
-      <hr />
+    <Winner winner={winner} /> 
+      
       { /* reset button */}
-      <button className="btn btn-danger"
-              onClick={ handleReset }>Reset</button>
+    <Button handleReset={handleReset} />
   </>
 );
 export default App;
