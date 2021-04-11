592223bdf670fcda917dc594672a5604f3a11207
Lottie <lottiej17@gmail.com>
2021-04-11 16:14:21 +0100

    Updated title on app page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import Button from './components/Button';
+import Reset from './components/Reset';
 import Winner from './components/Winner';
 import Player1 from './components/Player/Player1';
 import Player2 from './components/Player/Player2';
+
 
 const App = ({handleIncrementPlayer1, handleIncrementPlayer2, handleReset, serving, winner}) => (
     
   <>
       {/* header */}
       <header className="jumbotron mt-4 mb-0">
-          <h1>Ping Pong Party 🏓</h1>
+          <h1>Table Tennis Flip 🏓</h1>
       </header>
       
       {/* scores */}
@@ -26,10 +27,10 @@ const App = ({handleIncrementPlayer1, handleIncrementPlayer2, handleReset, servi
         </div>
           
       { /* winner message */}
-    <Winner winner={winner} /> 
+    <Winner /> 
       
       { /* reset button */}
-    <Button handleReset={handleReset} />
+    <Reset />
   </>
 );
 export default App;
